cdeb1ada6d55786bc59d5a6300e450d10b7dd447
ThiagoRAlmeida2 <thiagoralmeida23@gmail.com>
2024-11-29 10:39:21 -0300

    firebase. funcionando

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:login_register_app/firebase_options.dart';
+
 import 'login_register_app.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(statusBarIconBrightness: Brightness.light),
   );
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then(
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then(
     (_) => runApp(const LoginRegisterApp()),
   );
 }
